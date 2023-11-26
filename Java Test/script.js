e33cc060d25b95646573efbb5c36a47cb42c717d
swaxy21 <JOV230100@spengergasse.at>
2023-11-26 16:18:07 +0100

    6.Hausübung

--- Java Test/script.js
+++ Java Test/script.js
@@ -1,13 +1,26 @@
-
+let g, x;
+g= 5;
+x= 4;
 
 function myFunction1() {
-    document.getElementById("Button").innerHTML = 6+6   ;
+    document.getElementById("Button").innerHTML = 6*2  ;
     
 }
 
 function myFunction2() {
     window.alert("Nicht Drücken habe ich gesagt")
 }
+
+function myFunction3() {
+    window.print()
+ }
+
+function myFunction4() {
+    document.getElementById("demo").innerHTML = ("g");
+}
+
+
+
 
 
 
